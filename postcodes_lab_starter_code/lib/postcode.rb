69fafc032ed3f72fd36e91eb5c1f3fb4999d4f2a
Elliot Gagola <egagola@spartaglobal.com>
2018-11-28 09:31:59 +0000

    V.2.0 Generalised methods to be repeatable

--- postcodes_lab_starter_code/lib/postcode.rb
+++ postcodes_lab_starter_code/lib/postcode.rb
@@ -29,6 +29,14 @@ class Postcodesio
     @multiple_postcodes
   end
 
+  def get_element search_field , number_of_postcodes
+    if number_of_postcodes == 1
+      @single_postcode['result'][search_field]
+    elsif number_of_postcodes > 1
+      generate_array search_field
+    end
+  end
+
   def get_status number_of_postcodes
     if number_of_postcodes == 1
       @single_postcode['status']
@@ -64,142 +72,6 @@ class Postcodesio
         condensed_string_array << condensed_postcode
       end
       condensed_string_array
-    end
-  end
-
-  def get_quality_key number_of_postcodes, position
-    if number_of_postcodes == 1
-      @single_postcode["result"]["quality"]
-    elsif number_of_postcodes > 1
-      @multiple_postcodes['result'][position]['result']['quality']
-    end
-  end
-
-  def get_ordnance_survey_eastings number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['eastings']
-    elsif number_of_postcodes > 1
-      generate_array 'eastings'
-    end
-  end
-
-  def get_ordnance_survey_northings number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['northings']
-    elsif number_of_postcodes > 1
-      generate_array 'northings'
-    end
-  end
-
-  def get_country number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['country']
-    elsif number_of_postcodes > 1
-      generate_array 'country'
-    end
-  end
-
-  def get_NHS number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['nhs_ha']
-    elsif number_of_postcodes > 1
-      generate_array 'nhs_ha'
-    end
-  end
-
-  def get_longitude number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['longitude']
-    elsif number_of_postcodes > 1
-      generate_array 'longitude'
-    end
-  end
-
-  def get_latitude number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['latitude']
-    elsif number_of_postcodes > 1
-      generate_array 'latitude'
-    end
-  end
-
-  def get_parliamentary_constituency number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['parliamentary_constituency']
-    elsif number_of_postcodes > 1
-      generate_array 'parliamentary_constituency'
-    end
-  end
-
-  def get_european_electoral_region number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['european_electoral_region']
-    elsif number_of_postcodes > 1
-      generate_array 'european_electoral_region'
-    end
-  end
-
-  def get_primary_care_trust number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['primary_care_trust']
-    elsif number_of_postcodes > 1
-      generate_array 'primary_care_trust'
-    end
-  end
-
-  def get_region number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['region']
-    elsif number_of_postcodes > 1
-      generate_array 'region'
-    end
-  end
-
-  def get_parish number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['parish']
-    elsif number_of_postcodes > 1
-      generate_array 'parish'
-    end
-  end
-
-  def get_lsoa number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['lsoa']
-    elsif number_of_postcodes > 1
-      generate_array 'lsoa'
-    end
-  end
-
-  def get_msoa number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['msoa']
-    elsif number_of_postcodes > 1
-      generate_array 'msoa'
-    end
-  end
-
-  def get_admin_district number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['admin_district']
-    elsif number_of_postcodes > 1
-      generate_array 'admin_district'
-    end
-  end
-
-  def get_incode number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['incode']
-    elsif number_of_postcodes > 1
-      generate_array 'incode'
-    end
-  end
-
-  def get_outcode number_of_postcodes
-    if number_of_postcodes == 1
-      @single_postcode['result']['outcode']
-    elsif number_of_postcodes > 1
-      generate_array 'outcode'
     end
   end
 
--- postcodes_lab_starter_code/spec/postcode_spec.rb
+++ postcodes_lab_starter_code/spec/postcode_spec.rb
@@ -24,71 +24,71 @@ describe Postcodesio do
     end
 
     it "should return an quality key integer between 1-9" do
-      expect(@response.get_quality_key(1,0).to_i).to be_between(1,9).inclusive
+      expect(@response.get_element('quality',1).to_i).to be_between(1,9).inclusive
     end
 
     it "should return an ordnance survey eastings value as integer" do
-      expect(@response.get_ordnance_survey_eastings(1).class).to eq(Integer)
+      expect(@response.get_element('eastings',1).class).to eq(Integer)
     end
 
     it "should return an ordnance survey northings value as integer" do
-      expect(@response.get_ordnance_survey_northings(1).class).to eq(Integer)
+      expect(@response.get_element('northings',1).class).to eq(Integer)
     end
 
     it "should return a country which is one of the four constituent countries of the UK" do
-      expect(@response.get_country(1)).to eq("England").or eq("Scotland").or eq("Northern Ireland").or eq("Wales").or eq('Isle of Man')
+      expect(@response.get_element('country',1)).to eq("England").or eq("Scotland").or eq("Northern Ireland").or eq("Wales").or eq('Isle of Man')
     end
 
     it "should return a string value for NHS authority " do
-      expect(@response.get_NHS(1).class).to eq String
+      expect(@response.get_element('nhs_ha',1).class).to eq String
     end
 
     it "should return a longitude float value" do
-      expect(@response.get_longitude(1).class).to eq Float
+      expect(@response.get_element('longitude',1).class).to eq Float
     end
 
     it "should return a latitude float value" do
-      expect(@response.get_latitude(1).class).to eq Float
+      expect(@response.get_element('latitude',1).class).to eq Float
     end
 
     it "should return a parliamentary constituency string" do
-      expect(@response.get_parliamentary_constituency(1).class).to eq String
+      expect(@response.get_element('parliamentary_constituency',1).class).to eq String
     end
 
     it "should return a european_electoral_region string" do
-      expect(@response.get_european_electoral_region(1).class).to eq String
+      expect(@response.get_element('european_electoral_region',1).class).to eq String
     end
 
     it "should return a primary_care_trust string" do
-      expect(@response.get_primary_care_trust(1).class).to eq String
+      expect(@response.get_element('primary_care_trust',1).class).to eq String
     end
 
     it "should return a region string" do
-      expect(@response.get_region(1).class).to eq(String).or eq(NilClass)
+      expect(@response.get_element('region',1).class).to eq(String).or eq(NilClass)
     end
 
     it "should return a parish string" do
-      expect(@response.get_parish(1).class).to eq(String).or eq(NilClass)
+      expect(@response.get_element('parish',1).class).to eq(String).or eq(NilClass)
     end
 
     it "should return a lsoa string" do
-      expect(@response.get_lsoa(1).class).to eq String
+      expect(@response.get_element('lsoa',1).class).to eq String
     end
 
     it "should return a msoa string" do
-      expect(@response.get_msoa(1).class).to eq(String).or eq(NilClass)
+      expect(@response.get_element('msoa',1).class).to eq(String).or eq(NilClass)
     end
 
     it "should return a admin_district string" do
-      expect(@response.get_admin_district(1).class).to eq(String).or eq(NilClass)
+      expect(@response.get_element('admin_district',1).class).to eq(String).or eq(NilClass)
     end
 
     it "should return a incode string of three characters" do
-      expect(@response.get_incode(1).length).to eq 3
+      expect(@response.get_element('incode',1).length).to eq 3
     end
 
     it "should return a outcode string of 2-4 characters" do
-      expect(@response.get_outcode(1).length).to be_between(2,4).inclusive
+      expect(@response.get_element('outcode',1).length).to be_between(2,4).inclusive
     end
   end
 
@@ -123,104 +123,104 @@ describe Postcodesio do
     end
 
     it "should return an quality key integer between 1-9" do
-      for i in 0...3 do
-        expect(@response.get_quality_key(3,i)).to be_between(1,9).inclusive
+      @response.get_element('quality',3).each do |quality|
+        expect(quality).to be_kind_of Integer
       end
     end
 
     it "should return an ordnance survey eastings value as integer" do
-      @response.get_ordnance_survey_eastings(3).each do |easting|
+      @response.get_element('eastings',3).each do |easting|
         expect(easting).to be_kind_of Integer
       end
     end
 
     it "should return an ordnance survey northings value as integer" do
-      @response.get_ordnance_survey_northings(3).each do |northing|
+      @response.get_element('northings',3).each do |northing|
         expect(northing).to be_kind_of Integer
       end
     end
 
     it "should return a country which is one of the four constituent countries of the UK" do
-      @response.get_country(3).each do |easting|
-        expect(easting).to eq('England').or eq('Scotland').or eq('Northern Ireland').or eq('Wales')
+      @response.get_element('country',3).each do |country|
+        expect(country).to eq('England').or eq('Scotland').or eq('Northern Ireland').or eq('Wales')
       end
     end
 
     it "should return a string value for NHS authority " do
-      @response.get_NHS(3).each do |nhs|
+      @response.get_element('nhs_ha',3).each do |nhs|
         expect(nhs).to be_kind_of String
       end
     end
 
     it "should return a longitude float value" do
-      @response.get_longitude(3).each do |longitude|
+      @response.get_element('longitude',3).each do |longitude|
         expect(longitude).to be_kind_of Float
       end
     end
 
     it "should return a latitude float value" do
-      @response.get_latitude(3).each do |latitude|
+      @response.get_element('latitude',3).each do |latitude|
         expect(latitude).to be_kind_of Float
       end
     end
 
     it "should return a parliamentary constituency string" do
-      @response.get_parliamentary_constituency(3).each do |parliamentary_constituency|
+      @response.get_element('parliamentary_constituency',3).each do |parliamentary_constituency|
         expect(parliamentary_constituency).to be_kind_of(String).or be_kind_of(NilClass)
       end
     end
 
     it "should return a european_electoral_region string" do
-      @response.get_european_electoral_region(3).each do |european_electoral_region|
+      @response.get_element('european_electoral_region',3).each do |european_electoral_region|
         expect(european_electoral_region).to be_kind_of(String).or be_kind_of(NilClass)
       end
     end
 
     it "should return a primary_care_trust string" do
-      @response.get_primary_care_trust(3).each do |primary_care_trust|
+      @response.get_element('primary_care_trust',3).each do |primary_care_trust|
         expect(primary_care_trust).to be_kind_of(String).or be_kind_of(NilClass)
       end
     end
 
     it "should return a region string" do
-      @response.get_region(3).each do |region|
+      @response.get_element('region',3).each do |region|
         expect(region).to be_kind_of(String).or be_kind_of(NilClass)
       end
     end
 
     it "should return a parish string" do
-      @response.get_parish(3).each do |parish|
+      @response.get_element('parish',3).each do |parish|
         expect(parish).to be_kind_of(String).or be_kind_of(NilClass)
       end
     end
 
     it "should return a lsoa string" do
-      @response.get_lsoa(3).each do |lsoa|
+      @response.get_element('lsoa',3).each do |lsoa|
         expect(lsoa).to be_kind_of String
       end
     end
 
     it "should return a msoa string" do
-      @response.get_msoa(3).each do |msoa|
+      @response.get_element('msoa',3).each do |msoa|
         expect(msoa).to be_kind_of(String).or be_kind_of(NilClass)
       end
     end
 
     it "should return a admin_district string" do
-      @response.get_admin_district(3).each do |admin_district|
+      @response.get_element('admin_district',3).each do |admin_district|
         expect(admin_district).to be_kind_of(String).or be_kind_of(NilClass)
       end
     end
 
     it "should return a incode string of three characters" do
-      @response.get_incode(3).each do |incode|
+      @response.get_element('incode',3).each do |incode|
         expect(incode.length).to eq 3
       end
     end
 
 
     it "should return a outcode string of 2-4 characters" do
-      @response.get_outcode(3).each do |outcode|
+      @response.get_element('outcode',3).each do |outcode|
         expect(outcode.length).to be_between(2,4).inclusive
       end
     end
